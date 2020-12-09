fc54e9c6732c4fcc8da923a8b42d9032dba5fd67
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-12-09 21:39:21 +0100

    bug-bash

--- cloud-build-simple.sh
+++ cloud-build-simple.sh
@@ -9,4 +9,4 @@ VERSION=$(cat VERSION)
 #echo Proviamo a deployare la versione $VERSION.. Nota che sto script funge solo se chiamata dalla dir giusta.
 echo Scopiazzato da Sinattroccolo vediamo...
 time gcloud builds submit --config cloudbuild-bifido.yaml \
-    --substitutions=TAG_NAME="$VERSION",_DOCKER_VERSION="$VERSION",COMMIT_SHA="${APP_NAME}",REVISION_ID="2${APP_NAME}",REPO_NAME="3${APP_NAME},_APPNAME=${APP_NAME}cbs"
+    --substitutions=TAG_NAME="$VERSION",_DOCKER_VERSION="$VERSION",COMMIT_SHA="${APP_NAME}",REVISION_ID="2${APP_NAME}",REPO_NAME="3${APP_NAME},_APPNAME=${APPNAME}"
