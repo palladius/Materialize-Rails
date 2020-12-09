220275dec9a02b878a53d31ad6fe54409f36a41a
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-12-09 21:53:27 +0100

    bugfix

--- cloud-build-simple.sh
+++ cloud-build-simple.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # versione2 strafiga e automatica
-. .env
+source .env
 
 VERSION=$(cat VERSION)
-
+APP_NAME=${APPNAME:-credo-lingotto}
 
 #echo Proviamo a deployare la versione $VERSION.. Nota che sto script funge solo se chiamata dalla dir giusta.
-echo Scopiazzato da Sinattroccolo vediamo...
+
 time gcloud builds submit --config cloudbuild-bifido.yaml \
-    --substitutions=TAG_NAME="$VERSION",_DOCKER_VERSION="$VERSION",COMMIT_SHA="${APP_NAME}",REVISION_ID="2${APP_NAME}",REPO_NAME="3${APP_NAME},_APPNAME=${APPNAME}"
+    --substitutions=TAG_NAME="$VERSION",_DOCKER_VERSION="$VERSION",COMMIT_SHA="${APP_NAME}",REVISION_ID="2${APP_NAME}",REPO_NAME="3${APP_NAME}",_APPNAME="${APPNAME}"
